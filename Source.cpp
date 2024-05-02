8360bcc193deaefb54b498d99a726404abb4dea9
xReggimon <r-mcmullen.1@onu.edu>
2024-05-02 10:38:55 -0400

    Completed P13B-1.

--- Source.cpp
+++ Source.cpp
@@ -7,7 +7,32 @@ bool isLeapYear(int year);
 
 
 int main() {
+	string userInput;
+	int inputYear;
 
+	while (true) {
+		cout << "Enter a year or Q to quit: ";
+		cin >> userInput;
+
+		if (userInput == "Q" || userInput == "q") {
+			break;
+		}
+
+		try {
+			inputYear = stoi(userInput);
+
+			if (isLeapYear(inputYear)) {
+				cout << inputYear << " is a leap year." << endl;
+			}
+			else {
+				cout << inputYear << " is not a leap year." << endl;
+			}
+		}
+		catch (const invalid_argument& e) {
+			cout << "Invalid input." << endl;
+		}
+	}
+	return 0;
 	
 }
 
