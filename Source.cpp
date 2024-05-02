49694614c9610f7cb5b1e378d10586f9cba618b8
xReggimon <r-mcmullen.1@onu.edu>
2024-05-02 10:38:41 -0400

    Finished isLeapYear function.

--- Source.cpp
+++ Source.cpp
@@ -14,4 +14,5 @@ int main() {
 //This function determines whether a given year is a leap year under the Gregorian calendar
 bool isLeapYear(int year) {
 	return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+	//
 }
